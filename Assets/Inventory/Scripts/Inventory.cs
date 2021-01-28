1527d7d0c5e5b1657b0cd9a772a16085b88f2121
gabrielleo <gabrielleonardi2002@gmail.com>
2021-01-28 21:21:12 +0100

    Inventario + prefab punti di interesse

---
+++ Assets/Inventory/Scripts/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    [SerializeField] public GameObject[] items = new GameObject[6];
+    [SerializeField] KeyCode InventoryKey;
+    [SerializeField] public bool isInventoryOpen = false;
+    [SerializeField] public GameObject inventoryPopup;
+    [SerializeField] public Text itemDesc;
+    private string desc;
+    private int index = 0;
+    // Quando raccolco qualcosa
+    void PickUp(GameObject pick)
+    {
+        items[index] = pick;
+        index++;
+    }
+
+    //controllo per aprire l'inventario
+    void Update()
+    {
+        if(Input.GetKeyDown(InventoryKey) && isInventoryOpen == false)
+        {
+            inventoryPopup.SetActive(true);
+            isInventoryOpen = true;
+        }
+        else 
+        if (Input.GetKeyDown(InventoryKey) && isInventoryOpen == true)
+        {
+            inventoryPopup.SetActive(false);
+            isInventoryOpen = false;
+        }
+         
+    }
+
+    //controllo per il pickup
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "pickup")
+        {
+            PickUp(other.gameObject);
+        }
+
+        other.gameObject.SetActive(false);   
+    }
+
+    public void Descrizione(int index)
+    {
+        GameObject item = items[index];
+        desc = item.GetComponent<ItemInfo>().getDesc();
+        itemDesc.text = desc;
+    }
+
+}
---
+++ Assets/Inventory/Scripts/ItemInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInfo : MonoBehaviour
+{
+    public string description;
+    public Text ItemDesc;
+    public Image ItemImage;
+
+    public void PutDesc()
+    {
+        ItemDesc.text = description;
+    }
+
+    public string getDesc()
+    {
+        return description;
+    }
+}
