da10b575f6c0bb24f7a0363c63614df0f8c1724d
leolaz99 <leolaz99@gmail.com>
2021-01-28 12:00:57 +0100

    added menu and player movement

---
+++ Assets/Menu/Scripts/MenuManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject startingScreen;
+    public GameObject settings;
+    public GameObject credits;
+
+    public void GoToGameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void GoToStartingScreen()
+    {
+        startingScreen.SetActive(true);
+        settings.SetActive(false);
+    }
+
+    public void GoToSettings()
+    {
+        settings.SetActive(true);
+        startingScreen.SetActive(false);           
+    }
+
+    public void GoToCredits()
+    {
+        credits.SetActive(true);
+        startingScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
